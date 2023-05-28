2b2fe1555c5b270cad6ef3b328e34e0e484efca2
Art Genricx Alejandria <134836893+Haulester@users.noreply.github.com>
2023-05-28 17:28:58 +0800

    Add files via upload
    Attendance po maam

---
+++ Vehicle.java
@@ -0,0 +1,23 @@
+package attendance_23;
+
+public class Vehicle {
+
+void run() {
+   System.out.println("The vehicle is running");
+	}
+   void stop() {
+	   System.out.println("Stop");
+}
+   void Speed() {
+	   System.out.println("fast");
+}
+   void Color() {
+	   System.out.println("rainbow");
+}
+   void Price() {
+	   System.out.println("expensive");
+}
+      void model() {
+	   System.out.println("sikret");
+}
+}
