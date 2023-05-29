5fe23f0d88f39a435eaf11a941022b5135bb3375
Art Genricx Alejandria <134836893+Haulester@users.noreply.github.com>
2023-05-29 13:45:52 +0800

    Add files via upload

---
+++ Boat.java
@@ -0,0 +1,34 @@
+package attendance_23;
+
+public class Boat extends Vehicle {
+    @Override
+    void run() {
+        System.out.println("Boat is Floating");
+    }
+
+   
+    // Method to indicate the color of the boat
+    void color() {
+        System.out.println("Mainsail: orange");
+    }
+
+    // Method to indicate the speed of the boat
+    void Speed() {
+        System.out.println("Speed: 123124512mph");
+    }
+
+    // Method to indicate the color of the boat
+    void Color() {
+        System.out.println("Color: Violet");
+    }
+
+    // Method to indicate the price of the boat
+    void Price() {
+        System.out.println("Price: $192782131");
+    }
+
+    // Method to indicate the model of the boat
+    void model() {
+        System.out.println("Model: Fandango");
+    }
+}
